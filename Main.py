669d8a88e2709dfebe8574fd1cab7a6f151323b6
krishnag8596 <krishnagupta93462@gmail.com>
2023-10-28 12:10:16 +0530

    git commit -m

--- Main.py
+++ Main.py
@@ -2,16 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
 import datetime as dt
-#from playsound import playsound as ps
+from playsound import playsound as ps
 import webbrowser
 
 # CODE TO GET CURRENT TIME
 #print(dt.datetime.now())
 print("WELCOME TO WebDoc")
 
-user_name = str(input('Enter your Name :'))
-user_loc = str(input('Enter your complete adress in 3 lines: '))
-user_no = int(input('Enter your mobile no. :'))
+user_name = str(input('Enter your Name'))
+user_loc = str(input('Enter your complete adress in 3 lines '))
+user_no = int(input('Enter your mobile no.'))
 
 if user_name is None:
     print("INVALID")
@@ -21,9 +21,9 @@ elif user_no is None:
     print('INVALID')
 else:
     print("ACCOUNT CREATED")
-zz = 'yes'
+zz = 'y'
 z = []
-while zz == 'yes':
+while zz == 'y':
     print()
     print("1.SENIOR CITIZEN WELLNESS")
     print("2. TEEN HEALTH")
@@ -31,10 +31,10 @@ while zz == 'yes':
     print("4. HEALTH STATUS")
     print("5. AMBULANCE")
     print("6.WEBSITE")
-    ch1 = int(input('Select option according your requirement :'))
+    ch1 = int(input('Select option according your requirement'))
     if ch1==1:
-        c = 1
-        while c==1:
+        c = 'y'
+        while c=='y':
             print('SELECTED:SENIOR CITIZEN WELLNESS')
             print()
             print("1. BASIC DIAGNOSIS AND SUGGESTION")
@@ -47,21 +47,19 @@ while zz == 'yes':
                 print("We want some basic reports info of Yours for diagnosing you properly")
 
                 print()
-                BlP_Lu = eval(input("Enter your upper Blood pressure level:"))
-                BlP_Ll = eval(input("Enter your lower Blood pressure level:"))
-                Hrt_L = eval(input("Enter your Heart beat count:"))
+                BlP_Lu = eval(input("Enter your Blood pressure level"))
+                BlP_Ll = eval(input("Enter your Blood pressure level"))
+                Hrt_L = eval(input("Enter your Heart beat count"))
 
                 if 140 > BlP_Lu < 125:
                     print("Upper BP is not normal ")
                     print("SUGGESTED : 1. REDUCE SALT IN DIET,2.GET GOOD 8 HOURS SLEEP,3.REDUCE STRESS")
-                if 90 > BlP_Ll < 70:
+                elif 90 < BlP_Ll < 70:
                     print("Lower Bp is not normal")
                     print("SUGGESTED : 1. REDUCE SALT IN DIET,2.GET GOOD 8 HOURS SLEEP,3.REDUCE STRESS,4. REDUCE WEIGHT")
-                if 75 > Hrt_L > 128:
+                elif 75 > Hrt_L > 128:
                     print("IRREGULAR HEART RATE")
                     print("SUGGESTED : VISIT DOCTOR")
-                else:
-                    print('')
                 print('SELECT YOUR CONDITION')
                 print()
                 print("1.DIABETIC")
@@ -72,29 +70,25 @@ while zz == 'yes':
                     print("SUGGESTED:Eat foods that are rich in chromium and magnesium,reduce stress")
                 else:
                     print('NON DIABETIC')
-                c = int(input("Enter 1 to go back to Senior Citizens' page"))
-                if c != 1:
-                    zz = input("Enter yes to go to Main page:")
+                c = input("Enter y to go back to Senior Citizens' page")
             elif ch2 == 2:
                 print()
-                df=pd.read_csv('csvfile.csv')
+                df=pd.read_csv('C:\\Users\\krish\\OneDrive\\Documents\\csvfile.csv')
                 print("OUR DOCTORS LIST:")
                 print(df)
-                c = int(input("Enter 1 to go back to Senior Citizens' page"))
-                if c!=1:
-                    zz = input("Enter yes to go to Main page:")
+                c = input("Enter y to go back to Senior Citizens' page")
             elif ch2 == 3:
                 cc = 'y'
+                print("SELECTED:NUTRITON AND DIET")
+                print()
+                print("SELECT YOUR CONDITION")
+                print()
+                print("1.BP and Diabetes")
+                print("2.Diabetes")
+                print('3.BP')
+                print("4.NOTHING JUST NEED HEALTHY DIET")
+                ch3=int(input("SELECT:"))
                 while cc == 'y':
-                    print("SELECTED:NUTRITON AND DIET")
-                    print()
-                    print("SELECT YOUR CONDITION")
-                    print()
-                    print("1.BP and Diabetes")
-                    print("2.Diabetes")
-                    print('3.BP')
-                    print("4.NOTHING JUST NEED HEALTHY DIET")
-                    ch3 = int(input("SELECT:"))
                     if ch3==1:
                         print('You have BP and Diabetes')
                         print()
@@ -106,10 +100,6 @@ while zz == 'yes':
                         print()
                         print("FOR MORE  ASSISTANCE CONTACT OUR TEAM")
                         cc = input("Enter y to check other diets")
-                        if cc != 'y':
-                            c = int(input("Enter 1 to go back to Senior Citizens' page:"))
-                            if c != 1:
-                                zz = input("Enter yes to go to Main page:")
                     elif ch3==2:
                         print('You have Diabetes')
                         print()
@@ -121,10 +111,6 @@ while zz == 'yes':
                         print()
                         print("FOR MORE  ASSISTANCE CONTACT OUR TEAM")
                         cc = input("Enter y to check other diets")
-                        if cc != 'y':
-                            c = int(input("Enter 1 to go back to Senior Citizens' page:"))
-                            if c != 1:
-                                zz = input("Enter yes to go to Main page:")
                     elif ch3==3:
                         print('You have Blood pressure issue ')
                         print()
@@ -136,10 +122,6 @@ while zz == 'yes':
                         print()
                         print("FOR MORE  ASSISTANCE CONTACT OUR TEAM")
                         cc = input("Enter y to check other diets")
-                        if cc != 'y':
-                            c = int(input("Enter 1 to go back to Senior Citizens' page:"))
-                            if c != 1:
-                                zz = input("Enter yes to go to Main page:")
                     elif ch3==4:
                         print("HEALTHY DIET")
                         print()
@@ -149,21 +131,14 @@ while zz == 'yes':
                         print()
                         print("FOR MORE  ASSISTANCE CONTACT OUR TEAM")
                         cc = input("Enter y to check other diets")
-                        if cc != 'y':
-                            c = int(input("Enter 1 to go back to Senior Citizens' page:"))
-                            if c != 1:
-                                zz = input("Enter yes to go to Main page:")
                     else:
                         print('ERROR')
                         cc = input("Enter y to check other diets")
-                        if cc != 'y':
-                            c = int(input("Enter 1 to go back to Senior Citizens' page:"))
-                            if c != 1:
-                                zz = input("Enter yes to go to Main page:")
+                c = input("Enter y to go back to Senior Citizens' page")
             elif ch2==4:
                 print("REMINDER")
-                #df2=pd.read_csv('ambulan.csv')
-                #print(df2)
+                '''df2=pd.read_csv('C:\\Users\\krish\\OneDrive\\Documents\\ambulan.csv')
+                print(df2)'''
                 dose_n = int(input('Enter no of dosage you take in a day'))
                 if dose_n ==1:
                     rem_hour1 = int(input('Enter Hour'))
@@ -342,18 +317,62 @@ while zz == 'yes':
                                 exit
                 else:
                     exit
-                c = int(input("Enter 1 to go back to Senior Citizens' page"))
+                c = input("Enter y to go back to Senior Citizens' page")
             else:
                 exit
-                zz = input("Enter yes to go to main page:")
+        zz = input("Enter y to continue:")
+    elif ch1 == 2:
+        import matplotlib.pyplot as pl
+        print("Hello ! How has your Day been?")
+        a = int(input("1 : Stress Levels and Management\n2 : Nutrition\n3 : Consult a Doctor"))
+        if a == 1:
+            aa = 0
+            print("Please answer these few questions")
+            print(
+                "*Please note that this is not a diagnostic tool. Only a professional can identify the real issue\n1:Never to 5:Always")
+            x = int(input("Do you have trouble staying at the present moment?"))
+            aa += x
+            x = int(input("Do you struggle to fall asleep at night?"))
+            aa += x
+            x = int(input("On average, do you get less than 7-8 hours of sleep a night? "))
+            aa += x
+            x = int(input("Do you experience headaches or muscle tension?"))
+            aa += x
+            x = int(input("Do you have problem in concentrating in your tasks?"))
+            aa += x
+            if aa >= 21:
+                print("You show symptoms of HIGH LEVEL stress")
+                print("Please seek help and indulge in activities like Yoga and Meditation")
+                print("You are not alone")
+                if aa >= 25:
+                    print("Call our doctor and seek guidance. It's FREE :)")
+                    print("Dr.Keerthana : 7992739048")
+            if 10 < aa < 21:
+                print("You have shown symptoms of MODERATE stress levels")
+                if 10 < aa < 14:
+                    print("A moderate stress is required and is healthy. Maintain your stress in this level")
+                if 14 < aa < 21:
+                    print(
+                        "Though your levels seem to be moderate it is very close to high levels. Adopt habits like excercise,yoga and meditation.")
+            if aa <= 10:
+                print("You have LOW levels of stress")
+                if 1 <= aa <= 6:
+                    print(
+                        "Though Low levels are healthy and compromising, a little stress is required to have you driven in your life.")
+        xx = [(aa * 10 / 3), 100]
+        pl.pie(xx, labels=["Your stress", "Total"])
+        pl.show()
+
+        #if a == 2:
+        zz = input("Enter y to continue:")
     elif ch1==5:
         loc = user_loc
-        df2=pd.read_csv('ambulan.csv')
+        df2=pd.read_csv('C:\\Users\\krish\\OneDrive\\Documents\\ambulan.csv')
         #print(df2)
         print('sending help')
         print('ONE OF THE BELOW AMBULANCE WILL BE AT YOUR LOCATION IN 10 MIN')
         print(df2)
-        zz = input("Enter yes to go to main page:")
+        zz = input("Enter y to continue:")
     elif ch1 == 4:
         print('HEALTH STATUS')
         print()
@@ -431,7 +450,7 @@ while zz == 'yes':
                         print("YOUR CHILD IS SAFE AS HIS VACINATION IS COMPLETED")
                     elif m_ans=="no":
                         print("2 out of 3 done \n complete others asap \n doctors list:")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
                         #print("PLEASE TRY AGAIN LATER")
                     else:
@@ -441,12 +460,12 @@ while zz == 'yes':
                     m_ans = input("yes/no: ")
                     if m_ans =="yes" :
                         print("2 out of 3 Vaccines Completed \n DO COMPLETE REMAINING ASAP \n OUR DOCTORS:")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
                     elif m_ans=="no":
                         print("ONLY 1 Completed \n complete others as soon as possible")
                         print("DOCTORS LIST")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
             elif p_ans == "no":
                 print('IS your child tetanus vaccination completed?')
@@ -460,20 +479,20 @@ while zz == 'yes':
                         print("2 out of 3 done")
                         print()
                         print("COMPLETE OTHERS ASAP \n DOctors list")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
                 elif t_ans=="no":
                     print("Is your child measles vaccination done?")
                     m_ans = input("yes/no: ")
                     if m_ans =="yes" :
                         print("2 out of 3 Vaccines Completed \n DO COMPLETE REMAINING ASAP \n OUR DOCTORS:")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
                     else:
                         print("NONE COMPLETED")
                         print("VISIT DOCTOR ASAP")
                         print("DOCTORS")
-                        df3=pd.read_csv("vaccine.csv")
+                        df3=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\vaccine.csv")
                         print(df3)
         elif ch4==2:
             print("DO YOU NEED DIET FOR your CHILD")
@@ -497,7 +516,7 @@ while zz == 'yes':
             dd_ans = input("yes/no: ")
             if dd_ans=="yes":
                 print("DOCTORS LIST:")
-                df4=pd.read_csv("csvfile.csv")
+                df4=pd.read_csv("C:\\Users\\krish\\OneDrive\\Documents\\csvfile.csv")
                 print(df4)
             else:
                 print("Try Again later")
@@ -506,7 +525,7 @@ while zz == 'yes':
             print("Some error occured")
     if ch1==6:
         print("WEBSITE LINK")
-        webbrowser.open("http://www.google.com")
+        webbrowser.open("C:/Users/krish/Downloads/sharedrop-2023-10-28-06_26_46/Home.html")
     elif ch1 == 2:
         aaa = 0
         import matplotlib.pyplot as pl
@@ -545,30 +564,41 @@ while zz == 'yes':
                 if aa <= 10:
                     print("You have LOW levels of stress")
                     if 1 <= aa <= 6:
-                        print("Though Low levels are healthy and compromising, a little stress is required to have you driven in your life.")
-                xx = [(aa * 10 / 3), 100]
-                pl.pie(xx, labels=["Your stress", "Total"])
-                pl.show()
-                aaa = int(input("Enter 0 to go back to Teen health page:"))
-                if aaa != 0:
-                    zz = input("Enter yes to go back to main page:")
-            if a == 2:
-                print("The harsh truth is that Teens tend to eat more and more junk rather healthy food.\nThis can lead to high risk of health issues.")
-                print("To support your energy levels:\n1.Eat more grains rich in carbohydrates and proteins\n2.Eat more fibre\n3.A small cheat meal is healthy too!!")
-                aaa = int(input("Enter 0 to go back to Teen health page:"))
-                if aaa != 0:
-                    zz = input("Enter yes to go back to main page:")
-            if a == 3:
-                print()
-                df=pd.read_csv('csvfile.csv')
-                print("OUR DOCTORS LIST:")
-                print(df)
-                aaa = int(input("Enter 0 to go back to Teen health page:"))
-                if aaa != 0:
-                    zz = input("Enter yes to go back to main page:")
-            else :
-                print("Error")
-            aaa = int(input("Enter 0 to go back to Teen health page:"))
-            if aaa != 0:
-                zz = input("Enter yes to go back to main page:")
-print("Thank you for Using WebDoc!!")
+                        print(
+                            "Though Low levels are healthy and compromising, a little stress is required to have you driven in your life.")
+            xx = [(aa * 10 / 3), 100]
+            pl.pie(xx, labels=["Your stress", "Total"])
+            pl.show()
+            aaa = input("Enter y to go back to Teen health page:")
+        if a == 2:
+            print("The harsh truth is that Teens tend to eat more and more junk rather healthy food.\nThis can lead to high risk of health issues.")
+            print("To support your energy levels:\n1.Eat more grains rich in carbohydrates and proteins\n2.Eat more fibre\n3.A small cheat meal is healthy too!!")
+        if a == 3:
+            print()
+            df=pd.read_csv('C:\\Users\\krish\\OneDrive\\Documents\\csvfile.csv')
+            print("OUR DOCTORS LIST:")
+            print(df)
+        zz = input("Enter y to continue:")
+
+    else:
+        print('ERROR OCCURED')
+else:
+    print("ERROR OCCURED")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
